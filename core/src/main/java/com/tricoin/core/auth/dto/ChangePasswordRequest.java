9169250d6a8a7a137439e20a3c4e6854628dc05c
Sude Naz Aktas <snazaktas@gmail.com>
2026-07-16 03:23:57 +0300

    Add password change and avatar upload endpoints

---
+++ core/src/main/java/com/tricoin/core/auth/dto/ChangePasswordRequest.java
@@ -0,0 +1,9 @@
+package com.tricoin.core.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ChangePasswordRequest(
+    @NotBlank String currentPassword,
+    @NotBlank @Size(min = 6) String newPassword
+) {}
